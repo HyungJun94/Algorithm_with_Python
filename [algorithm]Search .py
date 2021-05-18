0723b6d1a75e3fe5b643772c7351f29cc48b178b
Hyung Jun Lim <83283400+HyungJun94@users.noreply.github.com>
2021-05-18 23:01:30 +0900

    Update [algorithm]Search .py

--- [algorithm]Search .py
+++ [algorithm]Search .py
@@ -212,3 +212,44 @@ def bfs(x,y):
 
 print(bfs(0,0))
 
+
+#In[4]:
+
+# [Baekjoon] 1260번: DFS와 BFS
+
+from collections import deque
+
+n,m,v = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+  i,j = list(map(int, input().split()))
+  graph[i].append(j)
+  graph[j].append(i)
+for i in range(n+1):
+  graph[i].sort()  
+
+def dfs(graph, v,visited):
+  visited[v] =True
+  print(v, end=' ')
+  for i in graph[v]:
+    if not visited[i]:
+      dfs(graph, i,visited)
+
+def bfs(graph, v,visited):
+  queue = deque([v])
+  visited[v]=True
+  while queue:
+    v = queue.popleft()
+    print(v, end = ' ')
+    for i in graph[v]:
+      if not visited[i]:
+        queue.append(i)
+        visited[i] =True 
+
+visited = [False]*(n+1)
+dfs(graph,v,visited)
+print()
+visited = [False]*(n+1)
+bfs(graph,v,visited)
